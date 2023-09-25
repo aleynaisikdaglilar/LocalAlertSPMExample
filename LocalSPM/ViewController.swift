caeb78c0f84c455735dc184057437a41c34985cf
aleynaisikdaglilar <aleynaisikdaglilar@gmail.com>
2023-09-25 12:41:18 +0300

    Initial Commit

---
+++ LocalSPM/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LocalSPM
+//
+//  Created by Aleyna Isikdaglilar [Ing Teknoloji A.S.-Tuzel Bankacilik Dijital Squad 1] on 25.09.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
