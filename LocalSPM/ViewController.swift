f9838a1865b155aa1fd0d2354a291f82e5895177
aleynaisikdaglilar <aleynaisikdaglilar@gmail.com>
2023-09-26 18:10:24 +0300

    first commit

--- LocalSPM/ViewController.swift
+++ LocalSPM/ViewController.swift
@@ -6,12 +6,34 @@
 //
 
 import UIKit
+import ButtonPackage
 
 class ViewController: UIViewController {
-
+    
+    lazy var button: UIButton = {
+        let button = UIButton()
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.backgroundColor = .blue
+        button.setTitle("Test Button", for: .normal)
+        button.addTarget(self, action: #selector(alert), for: .touchUpInside)
+        return button
+    }()
+    
+    @objc func alert() {
+        let alert = AlertPackage()
+        alert.alert(fromController: self)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        view.addSubview(button)
+        
+        button.widthAnchor.constraint(equalToConstant: 100).isActive = true
+        button.heightAnchor.constraint(equalToConstant: 50).isActive = true
+        
+        button.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        button.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
+        
     }
 
 
